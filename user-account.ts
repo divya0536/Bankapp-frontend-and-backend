ccb6abeb8ebfecbeea8bcffa2a17c6aa4ef235eb
divya0536 <69116559+divya0536@users.noreply.github.com>
2021-01-27 11:46:56 +0530

    Add files via upload

---
+++ user-account.ts
@@ -0,0 +1,12 @@
+// export class UserAccount {
+//     accountId: number;
+//     name: string;
+//     balance: number;
+//     dob: Date;
+//     accountType: string;
+//     address: string;
+//     city: string;
+//     phone: string;
+//     email: string;
+   
+// }
